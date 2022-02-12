ab7b8be1e27aa8baaef31781a5af309f021609b4
rikhii20 <rikhisobari22@gmail.com>
2022-02-13 02:11:30 +0800

    !

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
-module.exports.validate = (schema) => {
-    return async (req, res, next) => {
-      try {
-        const body = await req.body;
-=======
-const errorHandler = require("../utils/errorHandler")
+const errorHandler = require("../utils/errorHandler");
 
 module.exports.validate = (schema) => {
-    return (req, res, next) => {
-      try {
-        const body = req.body;
->>>>>>> 82648780c2b4a24f9722723c6ae0c118f33736e6
-        const { error } = schema.validate(body);
-        if (error)
-          return res.status(400).json({
-            status: "Bad Request",
-            message: error.message,
-          });
-        next();
-<<<<<<< HEAD
-      } catch (err) {
-        next(err);
-=======
-      } catch (error) {
-        errorHandler(error, res);
->>>>>>> 82648780c2b4a24f9722723c6ae0c118f33736e6
-      }
-    };
+  return (req, res, next) => {
+    try {
+      const body = req.body;
+      const { error } = schema.validate(body);
+      if (error)
+        return res.status(400).json({
+          status: "Bad Request",
+          message: error.message,
+        });
+      next();
+    } catch (error) {
+      errorHandler(error, res);
+    }
   };
-  
+};
