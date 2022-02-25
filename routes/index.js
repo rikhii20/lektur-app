f68e20838564c101a2a692932041be056569e730
Bayu Aji Pamungkas <bayu.aji51@ui.ac.id>
2022-02-25 10:49:58 +0700

    + assessment table relations, routes, and controller: create assessment, questions, options

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const contentRoutes = require("./content");
 const materialRoutes = require("./material");
 const studentRoutes = require("./student");
 const teacherRoutes = require("./teacher");
+const assessmentRoutes = require("./assessment");
 
 router.use("/course", courseRoutes);
 router.use("/content", contentRoutes);
@@ -13,5 +14,5 @@ router.use("/user", userRoutes);
 router.use("/material", materialRoutes);
 router.use("/student", studentRoutes);
 router.use("/teacher", teacherRoutes);
-
+router.use("/assessment", assessmentRoutes)
 module.exports = router;
